24573fd810afec188fc87a805232ff681930540d
kennyaapro23 <kennyaapro23@hotmail.com>
2025-04-10 11:43:34 -0500

    trabajo

---
+++ SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/controller/MarcaController.java
@@ -0,0 +1,19 @@
+package pe.edu.upeu.sysalmacen.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upeu.sysalmacen.Model.Marca;
+
+@RestController
+@RequestMapping("/marcas")
+public class MarcaController {
+    @GetMapping
+    public Marca mostrarMarca(){
+        return Marca.builder()
+                .idMarca(0L)
+                .nombre("adidas")
+                .build();
+
+    }
+}
