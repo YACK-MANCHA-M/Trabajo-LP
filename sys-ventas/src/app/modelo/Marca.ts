0307c8ebb88b3ba93425f3efa94728c019dfe49f
ChristianSC8 <christian.supo@upeu.edu.pe>
2025-05-29 11:08:18 -0500

    avance

---
+++ sys-ventas/src/app/modelo/Marca.ts
@@ -0,0 +1,9 @@
+export class Marca {
+  constructor(
+    public idMarca: number,
+    public nombre: string,
+
+  ) {}
+
+
+}
---
+++ sys-ventas/src/app/modelo/Message.ts
@@ -0,0 +1,12 @@
+export class Message {
+  constructor(
+    public statusCode: number,
+  public datetime: string,
+  public message: string,
+  public details: string,
+
+
+  ) {
+  }
+
+}
