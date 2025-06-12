3e57d2f4bd7c689da2fbae27a5d0c4559819fb85
ChristianSC8 <christian.supo@upeu.edu.pe>
2025-06-12 10:22:39 -0500

    trabajo

--- sys-ventas/src/app/paginas/pages.routes.ts
+++ sys-ventas/src/app/paginas/pages.routes.ts
@@ -2,20 +2,33 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {MatejemploComponent} from './matejemplo/matejemplo.component';
 import {MainMarcaComponent} from './main-marca/main-marca.component';
+import {FormMarcaComponent} from './main-marca/form-marca/form-marca.component';
+import {MainProductoComponent} from './main-producto/main-producto.component';
+import {FormxProductoComponent} from './main-producto/formx-producto/formx-producto.component';
+import {Not403Component} from './not403/not403.component';
+import {certGuard} from '../guard/cert.guard';
 
 export const pagesRoutes: Routes = [
-  { path: 'dashboard', component: DashboardComponent,  },
+  { path: 'dashboard', component: DashboardComponent, /*canActivate: [certGuard]*/ },
   { path: 'mattable', component: MatejemploComponent },
-  { path: 'marca', component: MainMarcaComponent },
-  /*{
-    path: 'product',
-    component: ListaProductoComponent,
-    children: [
-      { path: 'new', component: FormProductoComponent },
-      { path: 'edit/:id', component: FormProductoComponent },
+  { path: 'marca', component: MainMarcaComponent,
+    children:[
+      { path: 'new', component: FormMarcaComponent },
+      { path: 'edit/:id', component: FormMarcaComponent },
     ],
+    /*canActivate: [certGuard]*/
   },
-  { path: 'categoria', component: MainCategoriaComponent , },
+
+  {
+    path: 'product',
+    component: MainProductoComponent,
+    children: [
+      { path: 'new', component: FormxProductoComponent },
+      { path: 'edit/:id', component: FormxProductoComponent },
+    ], /*canActivate: [certGuard]*/
+  },
+  {path:'not-403', component: Not403Component}
+  /*{ path: 'categoria', component: MainCategoriaComponent , },
   //{ path: 'categoria', component: MainCategoriaComponent },
   {
     path: 'marca',
